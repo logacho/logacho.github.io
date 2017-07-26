f0e81f6ba9fd5434e793aa6fa7296b823a8c039a
logacho <kellylogacho2018@u.northwestern.edu>
2017-07-25 23:07:31 -0400

    Repaired routing and updated bootstrap theme

---
+++ src/app/app.routing.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+// Import components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component : HomeComponent
+  },
+  // {
+  //   path: 'projects',
+  //   component : ProjectsComponent
+  // },
+  // {
+  //   path: 'resume',
+  //   component : ResumeComponent
+  // },
+  // {
+  //   path: 'about',
+  //   component : AboutComponent
+  // },
+  // {
+  //    path: '**',
+  //    component : PageNotFoundComponent
+  // }
+  {
+     path: '**',
+     component : HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { enableTracing: false })],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
